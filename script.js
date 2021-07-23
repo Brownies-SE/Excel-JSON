11751c9978f5fd3ec0fa7a23152a044e5e627306
Josh Brown <brownjoshalan@gmail.com>
2021-07-23 10:20:00 -0600

    Now asks for the users input of the workbook and sheet

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const xlsx = require("xlsx");
+const prompt = require("prompt-sync")();
 
-const workbook = xlsx.readFile("Table4_AcreageInNewLeasesIssued_2020.xlsx", {
+const input = prompt("What is your Filename? ");
+console.log(`Filename is: ${input}`);
+
+const input2 = prompt("What is the sheet name?");
+
+const workbook = xlsx.readFile(`${input}`, {
   cellDates: true,
 });
-const worksheet = workbook.Sheets["Table 4 Acreage in New Leases "];
+const worksheet = workbook.Sheets[`${input2}`];
 
 const data = xlsx.utils.sheet_to_json(worksheet);
 
-//console.log(data);
+console.log(data);
 
 const newWorkBook = xlsx.utils.book_new();
 const newWorkSheet = xlsx.utils.json_to_sheet(data);
 xlsx.utils.book_append_sheet(newWorkBook, newWorkSheet, "New Data");
 
-xlsx.writeFile(newWorkBook, "New Data File.xlsx");
+//xlsx.writeFile(newWorkBook, "New Data File.xlsx");
